69fb674486086ca9b45eeda9b185aedfe7744a16
Linh Ngoc Le <linh.le@tuhh.de>
2022-10-23 15:42:42 +0000

    POC2

---
+++ approximate_majority_symmetric.py
@@ -0,0 +1,27 @@
+
+from ppsim import species, Simulation
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+def main():
+    a, b, u = 'A', 'B', 'U'
+    approximate_majority_symmetric = {
+        (a, b): (u, u), (b, a): (u, u),
+        (a, u): (a, a), (u, a): (a, a),
+        (b, u): (b, b), (u, b): (b, b)
+    }
+    n = 10 ** 9
+    init_config = {a: 0.501 * n, b: 0.499 * n}
+    sim = Simulation(init_config, approximate_majority_symmetric)
+    sim.run()
+    print(sim.history)
+    sim.history.plot()
+    plt.title('approximate majority protocol')
+    plt.xlim(0, sim.times[-1])
+    plt.ylim(0, n)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
